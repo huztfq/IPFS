c8fc316b6d2e2325640b40f940a621faef85417c
Huzaifa Tofeeq <75849337+huztfq@users.noreply.github.com>
2022-11-15 00:27:42 +0500

    Create prebuild.sh

---
+++ prebuild.sh
@@ -0,0 +1,5 @@
+apt install docker.io
+apt install docker-compose
+wget https://dist.ipfs.tech/ipfs-cluster-ctl/v1.0.4/ipfs-cluster-ctl_v1.0.4_linux-amd64.tar.gz
+tar xvzf ipfs-cluster-ctl_v1.0.4_linux-amd64.tar.gz
+docker-compose up
