c918b6890f0080f722da18bdfb9b43413ef5d0c1
Huzaifa Tofeeq <75849337+huztfq@users.noreply.github.com>
2022-12-04 17:51:11 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 apt install docker.io
 apt install docker-compose
+wget https://dist.ipfs.tech/ipfs-cluster-ctl/v1.0.4/ipfs-cluster-ctl_v1.0.4_linux-amd64.tar.gz
 tar xvzf ipfs-cluster-ctl_v1.0.4_linux-amd64.tar.gz
 cd ipfs-cluster-ctl
-wget https://dist.ipfs.tech/ipfs-cluster-ctl/v1.0.4/ipfs-cluster-ctl_v1.0.4_linux-amd64.tar.gz
 docker-compose up
