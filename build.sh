5df92f81808a424f2f4be8dfa3362292b2c6f253
Huzaifa Tofeeq <75849337+huztfq@users.noreply.github.com>
2023-01-20 07:10:30 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,24 @@
-apt install docker.io
-apt install docker-compose
-wget https://dist.ipfs.tech/ipfs-cluster-ctl/v1.0.4/ipfs-cluster-ctl_v1.0.4_linux-amd64.tar.gz
-tar xvzf ipfs-cluster-ctl_v1.0.4_linux-amd64.tar.gz
-cd ipfs-cluster-ctl
+#!/bin/bash
+
+sudo apt install docker.io
+sudo apt install docker-compose
+# download the latest version of IPFS
+wget https://dist.ipfs.io/go-ipfs/v0.7.0/go-ipfs_v0.7.0_linux-amd64.tar.gz
+
+# extract the downloaded archive
+tar xvfz go-ipfs_v0.7.0_linux-amd64.tar.gz
+
+# move the extracted files to a directory in PATH
+sudo mv go-ipfs/ipfs /usr/local/bin/
+
+# create a new IPFS repo
+ipfs init
+
+# configure the IPFS daemon to run in "offline" mode
+echo "--offline" > ~/.ipfs/config
+
+# start the IPFS daemon
+ipfs daemon &
+
+# create a new IPFS node
+ipfs bootstrap add /ip4/127.0.0.1/tcp/4001/ipfs/QmQ2r6iMNpky5f1m4cnm3Yqw8VSuUZ8DtQxnkZSJUcq6wR
