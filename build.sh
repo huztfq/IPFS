9900f94ce178536e363ee96bec20ff0746f8cd68
Huzaifa Tofeeq <75849337+huztfq@users.noreply.github.com>
2023-03-12 00:42:58 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,31 +1,11 @@
 #!/bin/bash
 
-# Install IPFS software
-wget https://dist.ipfs.io/go-ipfs/v0.9.1/go-ipfs_v0.9.1_linux-amd64.tar.gz
-tar -xvf go-ipfs_v0.9.1_linux-amd64.tar.gz
+# Download and extract IPFS software
+wget https://dist.ipfs.io/go-ipfs/v0.10.0/go-ipfs_v0.10.0_linux-amd64.tar.gz
+tar -xvf go-ipfs_v0.10.0_linux-amd64.tar.gz
+
+# Change directory to the IPFS folder
 cd go-ipfs
+
+# Install IPFS software
 sudo ./install.sh
-
-# Initialize IPFS node
-ipfs init --profile server
-
-# Check if fs-repo needs to be migrated
-if [ -e ~/.ipfs/api ]; then
-    ipfs daemon --migrate &
-else
-    ipfs daemon &
-fi
-
-# Wait for IPFS daemon to start
-sleep 5
-
-# Share content
-echo "Hello World!" > hello.txt
-ipfs add hello.txt
-
-# Set public gateway address
-ipfs config --json Addresses.Gateway '"/ip4/0.0.0.0/tcp/8080"'
-ipfs config --json Addresses.API '"/ip4/0.0.0.0/tcp/5001"'
-
-# Connect to other IPFS nodes
-ipfs swarm connect /dnsaddr/bootstrap.libp2p.io/ipfs/QmNnooDu7bfBzgo5b7y1LhRfg58ncK1zZquwbALqKGGg3y
